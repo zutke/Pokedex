b8e1467f3d8b66f17ff1df76e1c0bc5e135eefb1
Zachary Gutke <zachgutke@gmail.com>
2019-02-04 09:17:45 -0700

    Added Frame and Panel

---
+++ src/pokedex/controller/PokedexFrame.java
@@ -0,0 +1,16 @@
+package pokedex.controller;
+
+public class PokedexFrame
+{
+	private PokedexController app = new PokedexController();
+	private PokedexPanel panel = new PokedexPanel(app);
+	public PokedexFrame(PokedexController app)
+	{
+		
+	}
+	
+	public void setupFrame()
+	{
+		
+	}
+}
---
+++ src/pokedex/controller/PokedexPanel.java
@@ -0,0 +1,15 @@
+package pokedex.controller;
+
+import javax.swing.*;
+
+public class PokedexPanel
+{
+	private PokedexController app = new PokedexController();
+	private SpringLayout appLayout = new SpringLayout();
+	
+	public PokedexPanel(PokedexController app)
+	{
+		
+	}
+
+}
